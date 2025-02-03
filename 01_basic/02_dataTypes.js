351429e9936350e57a10bc8f667bbfabe770d967
Narendra Samtani <alenway97@gmail.com>
2025-02-03 22:28:15 +0530

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all Js code as newer version
+
+// alert("hello world") // will not work in node js
+
+let name = "nitin"
+let age = 99
+let isLoggedIn = true
+
+// number => 2^53
+// bigint =>
+// string => ""
+// boolean = true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// objects
+
+console.log(typeof "nitin")
