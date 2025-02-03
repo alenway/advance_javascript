d2b5f7e9f6da02ae9751e5872c616b131be815ae
Narendra Samtani <alenway97@gmail.com>
2025-02-03 22:28:08 +0530

    variables in javascript

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144523;
+let accountEmail = "alsidjflsjdfilj@gmail.com";
+let accountPassword = "1234";  // Changed `var` to `let`
+accountCity = "Ajmer";     // Explicitly declared `accountCity`//
+let accountState            // Default value will be `undefined`
+
+/*
+
+dont use var keyword
+
+*/
+
+
+// accountId = 3; // ❌ Not allowed (const can't be reassigned)
+accountEmail = "lsidf@gmail.com";
+accountPassword = "2394879";
+accountCity = "Pune";
+
+console.log(accountId);
+
+// Correcting console.table usage
+console.table({accountId, accountEmail, accountPassword, accountCity, accountState });
