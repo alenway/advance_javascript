b112c4b1eb01bd7ecd9fc7756e38c5092bd3fc3d
Narendra Samtani <alenway97@gmail.com>
2025-02-10 23:29:50 +0530

    reducer in foreach

---
+++ 05_iterators/eight.js
@@ -0,0 +1,37 @@
+const myNums = [1,2,3]
+
+// const myTotal = myNums.reduce(function (acc, currval){
+//     console.log(`acc: ${acc} and currval ${currval}`);
+//     return acc + currval
+// }, 0)
+
+// const myTotal = myNums.reduce((acc,curr)=>(acc+curr),0)
+
+// console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: 'js course',
+        price: 2999
+    },
+    {
+        itemName: 'java course',
+        price: 2999
+    },
+    {
+        itemName: 'python course',
+        price: 2999
+    },
+    {
+        itemName: 'webdev course',
+        price: 2999
+    },
+    {
+        itemName: 'devoops course',
+        price: 2999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc ,item) => (acc + item.price),0)
+
+console.log(priceToPay);
